10515568c3017d6beeb169f47c9d72d731bc7724
Jitlada Supreeyaporn <jitlada.su@ku.th>
2019-09-10 20:59:01 +0700

    add testcase

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,4 +1,5 @@
 import server from '../src/app'
+import config from 'config'
 
 describe("Server", () => {
 	it("isn't null", () => {
@@ -7,5 +8,9 @@ describe("Server", () => {
 
 	it("is defined", ()=> {
 		expect(server).toBeDefined()
+	})
+
+	it(`is running at ${config.get('port')}`, ()=> {
+		expect(server.address().port).toEqual(config.get('port'))
 	})
 })
